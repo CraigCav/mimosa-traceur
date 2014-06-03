59f1ca737f88b8b4640f361f27541c6d3a07512e
Ken Carpenter <ken@mindjuice.net>
2014-06-03 14:18:52 -0700

    Fixed Issue #2 and Issue #4
    Added support for passing through Traceur compiler flags.
    Added support for an exclude regex option to prevent Traceur from
    compiling certain JS files, such as third party or “vendor” files like
    underscore.js, backbone.js, etc.

--- src/config.js
+++ src/config.js
@@ -3,23 +3,85 @@
 exports.defaults = function() {
   return {
     traceur: {
+      exclude: [/\/vendor\//, /\/thirdparty\//],
       sourceMap: true,
       sourceMapDynamic: true,
       sourceMapExclude: [/\/specs?\//, /_spec.js$/],
-      sourceMapConditional: false
+      sourceMapConditional: false,
+      options: {
+        experimental: false,
+        annotations: false,
+        asyncFunctions: false,
+        arrowFunctions: false,
+        blockBinding: false,
+        classes: false,
+        computedPropertyNames: false,
+        defaultParameters: false,
+        destructuring: false,
+        forOf: false,
+        freeVariableChecker: false,
+        generatorComprehension: false,
+        generators: false,
+        modules: false,
+        numericLiterals: false,
+        propertyMethods: false,
+        propertyNameShorthand: false,
+        restParameters: false,
+        spread: false,
+        symbols: false,
+        templateLiterals: false,
+        typeAssertions: false,
+        types: false
+      }
     }
   };
 };
 
 exports.placeholder = function() {
   return "\t\n\n"+
-         "  traceur:                       # config settings for the traceur compiler module\n" +
-         "    sourceMap: true              # whether or not to create source maps\n" +
-         "    sourceMapDynamic: true       # whether or not to inline the source maps in the compiled JavaScript\n" +
-         "    sourceMapExclude: [/\\/specs?\\//, /_spec.js$/] # files to exclude from source map generation\n" +
-         "    sourceMapConditional: false  # whether or not to use conditional source maps\n"; +
-         "    experimental: false          # Turn on experimental features like 'let' block binding, arrow functions, etc."
+         "  traceur:                          # Config settings for the traceur compiler\n" +
+         "    exclude: [/\\/vendor\\//, /\\/thirdparty\\//]       # Files to exclude from traceur compilation\n" +
+         "    sourceMap: true                 # Whether or not to create source maps\n" +
+         "    sourceMapDynamic: true          # Whether or not to inline the source maps in the compiled JavaScript\n" +
+         "    sourceMapExclude: [/\\/specs?\\//, /_spec.js$/] # Files to exclude from source map generation\n" +
+         "    sourceMapConditional: false     # Whether or not to use conditional source maps\n" +
+         "    options:                        # Option flags for the traceur compiler\n" +
+         "      experimental: false           # Enable/disable all experimental features at once: true|false\n" +
+         "      annotations: false            # true|false|'parse'\n" +
+         "      asyncFunctions: false         # true|false|'parse'\n" +
+         "      arrowFunctions: false         # true|false|'parse'\n" +
+         "      blockBinding: false           # true|false|'parse'\n" +
+         "      classes: false                # true|false|'parse'\n" +
+         "      computedPropertyNames: false  # true|false|'parse'\n" +
+         "      defaultParameters: false      # true|false|'parse'\n" +
+         "      destructuring: false          # true|false|'parse'\n" +
+         "      forOf: false                  # true|false|'parse'\n" +
+         "      freeVariableChecker: false    # true|false\n" +
+         "      generatorComprehension: false # true|false|'parse'\n" +
+         "      generators: false             # true|false|'parse'\n" +
+         "      modules: false                # true|false|'parse'\n" +
+         "      numericLiterals: false        # true|false|'parse'\n" +
+         "      propertyMethods: false        # true|false|'parse'\n" +
+         "      propertyNameShorthand: false  # true|false|'parse'\n" +
+         "      restParameters: false         # true|false|'parse'\n" +
+         "      spread: false                 # true|false|'parse'\n" +
+         "      symbols: false                # true|false|'parse'\n" +
+         "      templateLiterals: false       # true|false|'parse'\n" +
+         "      typeAssertions: false         # true|false\n" +
+         "      types: false                  # true|false|'parse'\n";
 };
+
+// Validate that the given option is either a boolean or is a string with the value 'parse'
+var ifExistsIsTrueFalseParse = function(errors, fld, obj) {
+  if (obj) {
+    if ((typeof obj == "string") && (obj.toLowerCase() != "parse")) {
+      errors.push("" + fld + " must be true|false|'parse'");
+    }
+    else if (typeof obj != "boolean") {
+      errors.push("" + fld + " must be true|false|'parse'");
+    }
+  }
+}
 
 exports.validate = function(config, validators) {
   var errors = [];
@@ -44,9 +106,41 @@ exports.validate = function(config, validators) {
         config.traceur,
         'sourceMapExclude',
         config.watch.javascriptDir );
-
-      validators.ifExistsIsBoolean( errors, "traceur.experimental", config.traceur.experimental );
     }
+
+    validators.ifExistsFileExcludeWithRegexAndStringWithField(
+      errors,
+      "traceur.exclude",
+      config.traceur,
+      'exclude',
+      config.watch.javascriptDir );
+
+    // Validate true/false/parse options
+    ifExistsIsTrueFalseParse( errors, "traceur.options.annotations", config.traceur.options.annotations );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.asyncFunctions", config.traceur.options.asyncFunctions );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.arrowFunctions", config.traceur.options.arrowFunctions );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.blockBinding", config.traceur.options.blockBinding );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.classes", config.traceur.options.classes );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.computedPropertyNames", config.traceur.options.computedPropertyNames );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.defaultParameters", config.traceur.options.defaultParameters );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.destructuring", config.traceur.options.destructuring );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.forOf", config.traceur.options.forOf );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.generatorComprehension", config.traceur.options.generatorComprehension );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.generators", config.traceur.options.generators );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.modules", config.traceur.options.modules );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.numericLiterals", config.traceur.options.numericLiterals );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.propertyMethods", config.traceur.options.propertyMethods );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.propertyNameShorthand", config.traceur.options.propertyNameShorthand );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.restParameters", config.traceur.options.restParameters );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.spread", config.traceur.options.spread );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.symbols", config.traceur.options.symbols );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.templateLiterals", config.traceur.options.templateLiterals );
+    ifExistsIsTrueFalseParse( errors, "traceur.options.types", config.traceur.options.types );
+
+    // Validate boolean options
+    validators.ifExistsIsBoolean( errors, "traceur.options.experimental", config.traceur.options.experimental );
+    validators.ifExistsIsBoolean( errors, "traceur.options.freeVariableChecker", config.traceur.options.freeVariableChecker );
+    validators.ifExistsIsBoolean( errors, "traceur.options.typeAssertions", config.traceur.options.typeAssertions );
   }
 
   return errors;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ var _ = require('lodash'),
     config = require('./config'), 
     traceur = require('traceur');
 
+// Apply options from Mimosa to Traceur
+var setTraceurOptions = function ( traceur, options ) {
+  for (var optionName in options) {
+    if (options.hasOwnProperty(optionName)) {
+      // Only apply options that are truthy (e.g., true or 'parse')
+      if (options[optionName]) {
+        traceur.options[optionName] = options[optionName];
+      }
+    }
+  }
+}
+
+var isFileExcludedFromCompilation = function(fileName, excludeRegex) {
+  if (fileName.match( excludeRegex)) {
+    // This file is excluded from compilation by traceur
+    return true;
+  }
+  return false;
+}
+
 var compile = function (mimosaConfig, mimosaOptions, next) {
   var error, 
       options = _.extend( {}, mimosaConfig.traceur),
@@ -23,52 +43,57 @@ var compile = function (mimosaConfig, mimosaOptions, next) {
     }
   };
 
-  // Turn on/off the experimental features in Traceur
-  traceur.options.experimental = mimosaConfig.traceur.experimental;
+  setTraceurOptions( traceur, mimosaConfig.traceur.options );
 
   return mimosaOptions.files.forEach(function(file) {
-    // Check if source maps have been excluded for this file
-    if ( options.sourceMap ) {
-      if ( options.sourceMapExclude && options.sourceMapExclude.indexOf( file.inputFileName ) > -1 ) {
-        options.sourceMaps = false;
-      } else {
-        if ( options.sourceMapExcludeRegex && file.inputFileName.match( options.sourceMapExcludeRegex ) ) {
+    // Check if this file is excluded from compilation
+    if (isFileExcludedFromCompilation( file.inputFileName, mimosaConfig.traceur.excludeRegex )) {
+      // Do nothing for files that are excluded from compilation
+      file.outputFileText = file.inputFileText;
+    } else {
+      // Check if source maps have been excluded for this file
+      if ( options.sourceMap ) {
+        if ( options.sourceMapExclude && options.sourceMapExclude.indexOf( file.inputFileName ) > -1 ) {
           options.sourceMaps = false;
+        } else {
+          if ( options.sourceMapExcludeRegex && file.inputFileName.match( options.sourceMapExcludeRegex ) ) {
+            options.sourceMaps = false;
+          }
         }
       }
+
+      var reporter = new traceur.util.ErrorReporter(),
+          project = new traceur.semantics.symbols.Project('/'),
+          traceurOptions = {};
+
+      var sourceFile = new traceur.syntax.SourceFile(file.inputFileName, file.outputFileText || file.inputFileText);
+      project.addFile(sourceFile);
+
+      var compiledObjectMap = traceur.codegeneration.Compiler.compile(reporter, project, false);
+
+      if (reporter.hadError()) {
+        logger.error('Compilation failed - '+file.inputFileName, {
+          exitIfBuild: true
+        });
+        return;
+      }
+      
+      // No errors, so go ahead and generate the transformed code
+      if (options.sourceMaps) {
+        traceurOptions.sourceMapGenerator = new traceur.outputgeneration.SourceMapGenerator({
+          file: file.inputFileName
+        });
+      }
+
+      var writerConfig = {};
+      var result = traceur.outputgeneration.ProjectWriter.write(compiledObjectMap, writerConfig);
+
+      if (options.sourceMaps) {
+        file.sourceMap = traceurOptions.sourceMap;
+      }
+
+      file.outputFileText = result;
     }
-
-    var reporter = new traceur.util.ErrorReporter(),
-        project = new traceur.semantics.symbols.Project('/'),
-        traceurOptions = {};
-
-    var sourceFile = new traceur.syntax.SourceFile(file.inputFileName, file.outputFileText || file.inputFileText);
-    project.addFile(sourceFile);
-
-    var compiledObjectMap = traceur.codegeneration.Compiler.compile(reporter, project, false);
-
-    if (reporter.hadError()) {
-      logger.error('Compilation failed - '+file.inputFileName, {
-        exitIfBuild: true
-      });
-      return;
-    }
-
-    if (options.sourceMaps) {
-      traceurOptions.sourceMapGenerator = new traceur.outputgeneration.SourceMapGenerator({
-        file: file.inputFileName
-      });
-    }
-
-    var writerConfig = {};
-    var result = traceur.outputgeneration.ProjectWriter.write(compiledObjectMap, writerConfig);
-
-    if (options.sourceMaps) {
-      file.sourceMap = traceurOptions.sourceMap;
-    }
-
-    file.outputFileText = result;
-
     newFiles.push(file);
 
     done();
